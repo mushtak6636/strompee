7c4b89e765bb6354fa0282ec8673a74c6e37b468
Parabeac-Cloud <admin@parabeac.com>
2023-04-25 14:22:39 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/mushtak_theme.g.dart
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:mushtak/theme/mushtak_colors.g.dart';
+
+class MushtakTheme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: MushtakColors.lightOnPrimary,secondary: MushtakColors.lightSecondary);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: MushtakColors.darkPrimary,onPrimary: MushtakColors.darkOnPrimary);
+
+static  ThemeData themeDataDark = ThemeData( colorScheme: dark,);
+}
